a03642ecf296da4f5bd953986439e27164e37067
钱 启航 <1978980854@qq.com>
2025-03-28 21:44:22 +0800

    3月28日3 增加volume和volume effector

---
+++ Entity/EntityVolumeEffector.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class EntityVolumeEffector : MonoBehaviour
+{
+    public float velocityConversion = 1f;
+    public float accelerationMultiplier = 1f;
+    public float topSpeedMultiplier = 1f;
+    public float decelerationMultiplier = 1f;
+    public float turningDragMultiplier = 1f;
+    public float gravityMultiplier = 1f;
+
+    protected Collider m_collider;
+
+    protected virtual void Start()
+    {
+        m_collider = GetComponent<Collider>();
+        m_collider.isTrigger = true;
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Entity entity))
+        {
+            entity.velocity *= velocityConversion;
+            entity.accelerationMultiplier = accelerationMultiplier;
+            entity.topSpeedMultiplier = topSpeedMultiplier;
+            entity.decelerationMultiplier = decelerationMultiplier;
+            entity.turningDragMultipler = turningDragMultiplier;
+            entity.gravityMultiplier = gravityMultiplier;
+        }
+    }
+
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out Entity entity))
+        {
+            entity.accelerationMultiplier = 1f;
+            entity.topSpeedMultiplier = 1f;
+            entity.decelerationMultiplier = 1f;
+            entity.turningDragMultipler = 1f;
+            entity.gravityMultiplier = 1f;
+        }
+    }
+}
